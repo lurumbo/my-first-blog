bfa665e71bfdd4107f49bf7aeb755cea2d886e46
Lu Rumbo <lurumbo18@gmail.com>
2018-11-22 11:45:10 -0300

    My Django Girls app, first commit

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+def hi(name):
+	print('Hello ' + name + '!')
+
+#hi('Lu')
+
+girls = ['Rachel', 'Monica', 'Phoebe']
+for name in girls:
+	hi(name)
+	print('Next girl:')
---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lucia/projects/python/django/djangogirls/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
